2a059757a5d0cbd930286d0063c26c4c5b50f841
Daniel Costa <dangomescosta@gmail.com>
2020-09-13 09:23:25 -0300

    First commit

---
+++ To-Do-App/js/todo.js
@@ -0,0 +1,40 @@
+let toDo = []
+
+/*to add To-Do in the input field*/
+$("input").keypress(function(e){
+    if (e.which == 13) {
+        /* storing ToDo in a new variable */ 
+        let newToDo = $("input").val()
+        toDo.push(newToDo)
+
+        /*append new item */
+        $(".menu").append(`<div class="newToDo"> <span class="deleteToDo"> <i class="far fa-trash-alt can"></i> </span> <span class="textToDo">${toDo[(toDo.length-1)]}</span></div>`)
+
+        /* styling the new ToDo*/
+        $(".newToDo").even().addClass("even")
+        $(".newToDo").odd().addClass("odd")
+        $("#addTodo").val("")
+    }
+})
+
+/* to strikethrough and turn the item gray */
+$(".menu").on("click", ".textToDo", function(){
+    $(this).toggleClass("done")
+})
+
+/* to slide the delete button. Colt Steele does this effect with CSS. He sets the width of the bin span to 0 and then creates a new .deleteToDo:hover that it will transition to a larger width */
+$(".menu").on("mouseenter mouseleave", ".newToDo", function(){
+    $(this).find(".deleteToDo").slideToggle("500").css("display", "flex")
+})
+
+/* to delete the To-Do item*/
+$(".menu").on("click", ".deleteToDo", function(){
+    $(this).parent().remove()
+})
+
+/* to hide the add To-Do field */
+$(".hideInput").click(function(){
+    $("#addTodo").slideToggle("slow")
+})
+
+/* Things I learned: the importance of commenting the code. Specially what each function should do (specially to redo after a while). the differences between spand, li, ul - and to pick the right one. Stop propagation effect: when you set a click event to an element and you set a click event to its parent when you click in the element you will also trigger the click event of its parent. Bubble. Solution: function(event){event.stopPropagation()}*/
